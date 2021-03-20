5cd1f5e9fc3ca6441009e592814c82ace71bb67a
Jef Spaleta <jspaleta@gmail.com>
2021-03-19 23:18:05 -0800

    working federation metrics query

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jspaleta/sensu-dashboard-metrics
 go 1.14
 
 require (
+	github.com/machinebox/graphql v0.2.2
 	github.com/sensu-community/sensu-plugin-sdk v0.11.0
 	github.com/sensu/sensu-go/api/core/v2 v2.3.0
 	github.com/sensu/sensu-go/types v0.3.0
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	"github.com/sensu/sensu-go/types"
-	"github.com/shurcooL/graphql"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -23,6 +23,54 @@ type Config struct {
 }
 
 var (
+	fedQueryData = map[string]string{"query": `{
+  clusters: forward {
+    ... on Query {
+      name: clusterName
+      metrics: clusterMetrics {
+        ... on ClusterMetrics {
+          namespaces {
+            name
+            eventGauges {
+              total
+              statusCritical
+              statusWarning
+              statusOther
+              statusOK
+              statePassing
+              stateFailing
+            }
+            keepaliveGauges {
+              total
+              statusCritical
+              statusWarning
+              statusOther
+              statusOK
+              statePassing
+              stateFailing
+            }
+            entityGauges {
+              total
+              agent
+              proxy
+              other
+            }
+          }
+          clusterGauges {
+            total
+          }
+        }
+        ... on FetchErr {
+          code
+          message
+        }
+      }
+    }
+    ... on ForwardErr {
+      errName: name
+      errMsg: err
+    }
+  }}`}
 	plugin = Config{
 		PluginConfig: sensu.PluginConfig{
 			Name:     "sensu-dashboard-metrics",
@@ -37,7 +85,7 @@ var (
 			Env:       "DASHBOARD_URL",
 			Argument:  "url",
 			Shorthand: "u",
-			Default:   "http://localhost:3000/",
+			Default:   "http://localhost:3000/graphql",
 			Usage:     "Url to access the Sensu Dashboard Web-UI",
 			Value:     &plugin.Url,
 		},
@@ -69,70 +117,6 @@ var (
 	}
 )
 
-//{ versions { backend { version } } }
-var query struct {
-	Versions struct {
-		Backend struct {
-			Version graphql.String
-		}
-	}
-}
-
-/* This should match graphiql query:
-query ClusterMetrics {
-    forward {
-      __typename
-      ... on Query {
-      	clusterName
-      }
-
-    }
-}
-
-*/
-
-// golang client Ref: https://github.com/shurcooL/graphql#readme
-var federationQuery struct { // Top level golang var representing the query
-	ClusterMetrics struct {
-		forward struct {
-			__typename graphql.String
-			Query      struct {
-				clusterName graphql.String
-			} `graphql:"... on Query"` //instruct golang client that this is an inline fragment
-		}
-	}
-}
-
-/* TODO: the rest of the query structure to add
-clusterMetrics struct {
-	namespaces struct {
-		name        graphql.String
-		eventGauges struct {
-			total          graphql.Int
-			statusCritical graphql.Int
-			statusWarning  graphql.Int
-			statusOther    graphql.Int
-			statusOK       graphql.Int
-			statePassing   graphql.Int
-			stateFailing   graphql.Int
-		}
-		keepaliveGauges struct {
-			total    graphql.Int
-			statusOK graphql.Int
-		}
-		entityGauges struct {
-			total graphql.Int
-			agent graphql.Int
-			proxy graphql.Int
-			other graphql.Int
-		}
-	}
-	clusterGauges struct {
-		total graphql.Int
-	}
-} `graphql:"... on ClusterMetrics"`
-*/
-
 func main() {
 	check := sensu.NewGoCheck(&plugin.PluginConfig, options, checkArgs, executeCheck, false)
 	check.Execute()
@@ -148,7 +132,87 @@ func checkArgs(event *types.Event) (int, error) {
 	return sensu.CheckStateOK, nil
 }
 
+type versionQuery struct {
+	Data struct {
+		Versions struct {
+			Backend struct {
+				Version string
+			}
+		}
+	}
+}
+
+type entityGauges struct {
+	Agent int
+	Other int
+	Proxy int
+	Total int
+}
+
+type eventGauges struct {
+	StateFailing   int
+	StatePassing   int
+	StatusCritical int
+	StatusOK       int
+	StatusOther    int
+	StatusWarning  int
+	Total          int
+}
+
+type keepaliveGauges struct {
+	StateFailing   int
+	StatePassing   int
+	StatusCritical int
+	StatusOK       int
+	StatusOther    int
+	StatusWarning  int
+	Total          int
+}
+
+type federationMetricsQuery struct {
+	Auth bool
+	Data struct {
+		Clusters []struct {
+			Name    string
+			Metrics struct {
+				ClusterGauges struct {
+					Total int
+				}
+				Namespaces []struct {
+					Name            string
+					EntityGauges    entityGauges
+					EventGauges     eventGauges
+					KeepaliveGauges keepaliveGauges
+				}
+			}
+			ErrName string
+			ErrMsg  string
+		}
+	}
+}
+
 func executeCheck(event *types.Event) (int, error) {
+	//version query
+	jsonData := map[string]string{
+		"query": `{ versions { backend { version } } }`,
+	}
+	data, err := graphqlQuery(jsonData)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return sensu.CheckStateCritical, nil
+	}
+	var result versionQuery
+	json.Unmarshal([]byte(data), &result)
+	fmt.Printf("result: %+v\n", result)
+
+	data, err = graphqlQuery(fedQueryData)
+	var fresult federationMetricsQuery
+	json.Unmarshal([]byte(data), &fresult)
+	fmt.Printf("fresult: %+v\n", fresult)
+	return sensu.CheckStateOK, nil
+}
+
+func graphqlQuery(queryStr map[string]string) ([]byte, error) {
 	roots := x509.NewCertPool()
 	if len(plugin.CAFile) > 0 {
 		rootPEM, err := ioutil.ReadFile(plugin.CAFile)
@@ -161,36 +225,31 @@ func executeCheck(event *types.Event) (int, error) {
 		}
 	}
 	httpclient := &http.Client{
-		Transport: &transport{
-			underlyingTransport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: plugin.SkipVerify,
-					RootCAs:            roots,
-				},
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: plugin.SkipVerify,
+				RootCAs:            roots,
 			},
 		},
 	}
-
-	gqlclient := graphql.NewClient("https://localhost:3000/graphql", httpclient)
-	err := gqlclient.Query(context.Background(), &query, nil)
+	jsonValue, _ := json.Marshal(queryStr)
+	//fmt.Println(string(jsonValue))
+	request, err := http.NewRequest("POST", plugin.Url, bytes.NewBuffer(jsonValue))
 	if err != nil {
-		fmt.Printf("Error doing query: %v", err)
+		fmt.Printf("The HTTP request build failed with error %s\n", err)
+		return nil, err
 	}
-	fmt.Println(query.Versions.Backend.Version)
-	err = gqlclient.Query(context.Background(), &federationQuery, nil)
-	if err != nil {
-		fmt.Printf("Error doing query: %v", err)
+	request.Header.Add("Authorization", "Key "+plugin.ApiKey)
+	request.Header.Add("Content-Type", "application/json")
+	response, err := httpclient.Do(request)
+	if response == nil || err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return nil, err
 	}
-	return sensu.CheckStateOK, nil
-}
+	defer response.Body.Close()
+	//fmt.Println(response.Status)
 
-type transport struct {
-	underlyingTransport http.RoundTripper
-}
-
-func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
-	if len(plugin.ApiKey) > 0 {
-		req.Header.Add("Authorization", "Key "+plugin.ApiKey)
-	}
-	return t.underlyingTransport.RoundTrip(req)
+	data, _ := ioutil.ReadAll(response.Body)
+	//fmt.Println(string(data))
+	return []byte(data), err
 }
